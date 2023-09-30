a7517152b43566999b3337449df3064f636bf5d0
itsmedantheman <winback9@proton.me>
2023-09-30 17:34:52 +0200

    setup hello-world

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ npm i
 npm run dev
 ```
 
-Run npm-package update
+update npm-package
 
 ```bash
-npx npm-check-updates
-npx npm-check-updates -u
+npm outdated
+npm update
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero__main">
+      <div className="flex-1 p-x">
+        <h1 className="hero__title">Hello World!</h1>
+        <p className="hero__subtitle">This gets awesome. To be continued...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
